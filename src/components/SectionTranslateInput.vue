9c32016a172a1f1245ceb0cc846fb9d163e1c4dc
alessandrostorchi92 <storchi.alle@gmail.com>
2024-06-11 01:11:51 +0200

    translate btn ok!

--- src/components/SectionTranslateInput.vue
+++ src/components/SectionTranslateInput.vue
@@ -10,8 +10,62 @@ export default {
 
 <template>
 
+    <div class="language-select">
+
+        <select id="options" name="options">
+
+            <option value="Spanish">🇪🇸</option>
+            <option value="English">🇬🇧</option>
+            <option value="French">🇫🇷</option>
+            <option value="German">🇩🇪</option>
+            <option value="Portuguese">🇵🇹</option>
+
+        </select>
+
+    </div>
+
+    <div class="position-translate-btn">
+        <button type="button" class="btn btn-primary translate-btn">Translate</button>
+    </div>
 
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use 'src/styles/partials/mixins.scss' as *;
+
+.language-select {
+
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding-top: 2rem;
+
+}
+
+select {
+
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    height: 2.6rem;
+    width: 3.6rem;
+    border-radius: 8px;
+    font-size: 1.8rem;
+
+}
+
+option {
+    text-align: center;
+}
+
+.position-translate-btn {
+    position: relative;
+}
+
+.translate-btn {
+    margin-top: 3.5rem;
+    @include position-center;
+}
+</style>
